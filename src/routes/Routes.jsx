ee9ef8f965b34c92f2c5718a790a4a7467574ddc
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2022-07-13 16:56:40 -0500

    Created React project and Footer component

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,10 @@
+const { BrowserRouter } = require("react-router-dom");
+
+
+return <BrowserRouter>
+        <Routes>
+            <Route/>
+        </Routes>
+    </BrowserRouter>
+
+export default Routes
